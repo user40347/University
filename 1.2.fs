1fc0a5b1466d8e1b46fe87b6992e5a20d63f33b2
user40347 <140146157+user40347@users.noreply.github.com>
2025-03-06 16:02:26 +0500

    Add files via upload

---
+++ 1.2.fs
@@ -0,0 +1,23 @@
+﻿open System
+printf"Введите число: "
+let s = Console.ReadLine()
+
+
+let testInt (str:string) = [
+    for i in 0..str.Length-1 do
+        match str[i] with
+        | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> yield int((s[i]).ToString())
+        | _ ->
+            printf"Это не натуральное число. Программа завершена"
+            Environment.Exit(0)
+        ]
+        
+ 
+let sumList list =
+    let rec loop acc = function
+        | [] -> acc
+        | head :: tail -> loop (acc + head) tail
+    loop 0 list
+
+printf "Послед %A" (sumList (testInt s))
+
